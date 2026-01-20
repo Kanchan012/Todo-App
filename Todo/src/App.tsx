4e4f867f7feecec3a145bef0f6ad556a27db7884
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-01-20 18:24:51 +0545

    updated app.tsx

--- Todo/src/App.tsx
+++ Todo/src/App.tsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
-import { TodoProvider } from "./context/TodoProvider";
+import AllTasks from "./pages/AllTasks";
+import CompletedTasks from "./pages/CompletedTasks";
+import IncompleteTasks from "./pages/IncompleteTasks";
 
 export default function App() {
   return (
-    <TodoProvider>
+    <BrowserRouter>
+      <Navbar />
       <TodoForm />
-    </TodoProvider>
+      <Routes>
+        <Route path="/" element={<AllTasks />} />
+        <Route path="/completed" element={<CompletedTasks />} />
+        <Route path="/incomplete" element={<IncompleteTasks />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
