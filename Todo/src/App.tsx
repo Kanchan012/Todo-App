e8acc0e42c80c2028e33c5989fc37993308197ea
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-01-20 18:40:33 +0545

    updated app.tsx and index.css

--- Todo/src/App.tsx
+++ Todo/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
@@ -5,16 +6,27 @@ import AllTasks from "./pages/AllTasks";
 import CompletedTasks from "./pages/CompletedTasks";
 import IncompleteTasks from "./pages/IncompleteTasks";
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <TodoForm />
-      <Routes>
-        <Route path="/" element={<AllTasks />} />
-        <Route path="/completed" element={<CompletedTasks />} />
-        <Route path="/incomplete" element={<IncompleteTasks />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Todo Form */}
+        <TodoForm />
+
+        {/* Main Content */}
+        <main className="flex-1 p-4 max-w-2xl mx-auto w-full">
+          <Routes>
+            <Route path="/" element={<AllTasks />} />
+            <Route path="/completed" element={<CompletedTasks />} />
+            <Route path="/incomplete" element={<IncompleteTasks />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
-}
+};
+
+export default App;
