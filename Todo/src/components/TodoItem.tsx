de5871b8298b6af3f3abf071402f14b9303c52fc
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-01-20 19:30:53 +0545

    todoitem updated

--- Todo/src/components/TodoItem.tsx
+++ Todo/src/components/TodoItem.tsx
@@ -1,36 +1,25 @@
 import React from "react";
-import { useTodos } from "../context/TodoContext";
 import type { Todo } from "../types/todo";
+import { useTodos } from "../context/TodoContext";
 
-interface TodoItemProps {
+interface Props {
   todo: Todo;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+const TodoItem: React.FC<Props> = ({ todo }) => {
   const { toggleTodo, deleteTodo } = useTodos();
 
   return (
-    <div
-      className={`todo-item ${todo.completed ? "completed" : ""}`}
-    >
-      <span className="title">{todo.title}</span>
-      <span className={`priority priority-${todo.priority}`}>
-        {todo.priority}
-      </span>
-      <div>
-        <button
-          className="toggle-btn"
-          onClick={() => toggleTodo(todo.id)}
-        >
-          {todo.completed ? "Undo" : "Done"}
-        </button>
-        <button
-          className="delete-btn"
-          onClick={() => deleteTodo(todo.id)}
-        >
-          Delete
-        </button>
+    <div className={`todo-item ${todo.completed ? "completed" : ""}`}>
+      <div className={`title priority-${todo.priority}`}>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => toggleTodo(todo.id)}
+        />
+        {todo.title}
       </div>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
     </div>
   );
 };
