26785874948eee3bcb2a70368127ab041efc2aab
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-01-21 15:35:02 +0545

    importing reusable components with props

--- Todo/src/components/TodoForm.tsx
+++ Todo/src/components/TodoForm.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { useTodos } from "../context/TodoContext";
 import type { Priority } from "../types/todo";
+import Input from "./ui/Input";
+import Select from "./ui/Select";
+import Button from "./ui/Button";
 
 const TodoForm: React.FC = () => {
   const { addTodo } = useTodos();
@@ -17,21 +20,23 @@ const TodoForm: React.FC = () => {
 
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
-      <input
+      <Input
         type="text"
         placeholder="Enter task..."
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
-      <select
+     <Select
         value={priority}
         onChange={(e) => setPriority(e.target.value as Priority)}
-      >
-        <option value="low">Low Priority</option>
-        <option value="medium">Medium Priority</option>
-        <option value="high">High Priority</option>
-      </select>
-      <button type="submit">Add Task</button>
+        options={[
+          { value: "low", label: "Low Priority" },
+          { value: "medium", label: "Medium Priority" },
+          { value: "high", label: "High Priority" },
+        ]}
+      />
+
+      <Button type="submit">Add Task</Button>
     </form>
   );
 };
